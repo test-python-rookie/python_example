1049f8eb43f1580a8f56c36f06c3aa6f61fa1860
test-python-rookie <13929751497@163.com>
2020-12-24 10:28:13 +0800

    修改

--- No02_冒泡排序.py
+++ No02_冒泡排序.py
@@ -1,12 +1,15 @@
-def maopao():
-    list = [1,2,3,4,5,6,7,8]
-    for j in range(len(list)):
-        for i in range(len(list)-1-j):
+# 冒泡排序
+def maopao(list):
+    for j in range(1, len(list)):
+        for i in range(len(list)-j):
             if list[i] > list[i+1]:
                 list[i], list[i+1] = list[i+1], list[i]
             else:
                 continue
-    print(list)
+    print("冒泡排序后列表：", list)
+
 
 if __name__ == "__main__":
-    maopao()
+    mp = [11, 21, 13, 24, 35, 16, 37, 28]
+    print("冒泡排序前列表：", mp)
+    maopao(mp)
