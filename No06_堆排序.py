15bbb658177eff4e22f338fbcfb0fdd54d6cc1c3
test-python-rookie <13929751497@163.com>
2020-12-25 10:27:14 +0800

    先上传，看不懂

---
+++ No06_堆排序.py
@@ -0,0 +1,36 @@
+# 堆排序
+def buildMaxHeap(arr):
+    import math
+    for i in range(math.floor(len(arr)/2),-1,-1):
+        heapify(arr,i)
+
+def heapify(arr, i):
+    left = 2*i+1
+    right = 2*i+2
+    largest = i
+    if left < arrLen and arr[left] > arr[largest]:
+        largest = left
+    if right < arrLen and arr[right] > arr[largest]:
+        largest = right
+    if largest != i:
+        swap(arr, i, largest)
+        heapify(arr, largest)
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+def heapSort(arr):
+    global arrLen
+    arrLen = len(arr)
+    buildMaxHeap(arr)
+    for i in range(len(arr)-1,0,-1):
+        swap(arr,0,i)
+        arrLen -=1
+        heapify(arr, 0)
+    return arr
+
+if __name__ == '__main__':
+    list = [1, 5, 8, 123, 22, 54, 7, 99, 300, 222]
+    print("List source is:", list)
+    result = heapSort(list)
+    print("List sort is:", result)
