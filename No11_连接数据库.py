0052bb8aceae95439bddae4e3e8132115352ed34
test-python-rookie <13929751497@163.com>
2020-12-30 14:20:54 +0800

    导入PrettyTable库，以表格的形式显示数据表内容

--- No11_连接数据库.py
+++ No11_连接数据库.py
@@ -1,21 +1,27 @@
 import mysql.connector
+from prettytable import PrettyTable
+
 host = "localhost"
 user = "root"
 password = "123456"
 database="test"
 mydb = mysql.connector.connect(host = host, user = user, password = password, database = database)
 mycursor = mydb.cursor()
-str = """SELECT s.name AS 姓名, 
-			CASE s.sex 
-				WHEN 1 THEN "男"
-				WHEN 2 THEN "女"
-				WHEN 0 THEN "未知"
-			END AS 性别, 
-			s.age AS 年龄, 
-			group_concat(k.km separator '/') AS 科目
-            FROM student s
-            JOIN (SELECT * FROM student_kemu s JOIN kemu k ON s.kemuid = k.id) k ON s.id = k.studentid
-            GROUP BY s.name, s.sex, s.age"""
+str = """
+SELECT s.name AS 姓名, 
+        CASE s.sex 
+            WHEN 1 THEN "男"
+            WHEN 2 THEN "女"
+            WHEN 0 THEN "未知"
+        END AS 性别, 
+        CAST(s.age AS CHAR) AS 年龄, 
+        group_concat(k.km separator '/') AS 科目
+FROM student s
+JOIN (SELECT * FROM student_kemu s JOIN kemu k ON s.kemuid = k.id) k ON s.id = k.studentid
+GROUP BY s.name, s.sex, s.age
+"""
 mycursor.execute(str)
+table = PrettyTable(("姓名", "性别", "年龄", "科目"))
 for i in mycursor:
-    print(i)
+    table.add_row(i)
+print(table)
